13e7c69af2707f58f81ace11e1c6582c14861407
dejsan <dexan.maric@gmail.com>
2018-10-26 01:30:38 +0200

    App container -Removed navigation links

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,23 +1,14 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Blog from '../blog'
 import AddPost from '../addPost'
 import './index.css'
 
 const App = () => (
-    <div class="container text-center">
-
-        <header>
-            <Link to="/">Blog</Link>
-            <Link to="/add-post">Add Post</Link>
-        </header>
-
-        <main>
-            <Route exact path="/" component={Blog} />
-            <Route exact path="/add-post" component={AddPost} />
-        </main>
-
-    </div>
+    <main class="container text-center">
+        <Route exact path="/" component={Blog} />
+        <Route exact path="/add-post" component={AddPost} />
+    </main>
 )
 
 export default App
